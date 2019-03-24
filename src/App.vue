c8eb72a5679a742b84c673ef81aa2cb97deefbeb
michaeltrilford <trilfordm@gmail.com>
2019-03-24 21:27:49 +1100

    Styled the application

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
+
+body {
+  background: $dark-blue;
+  margin: 0;
+}
+
+h1 {
+  @include heading-1;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background: black;
+  color: $white;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $white;
     &.router-link-exact-active {
       color: #42b983;
     }
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <p class="request">
+      Don't have a Design+HQ account?
+      <router-link to="/request">Request an account</router-link>
+    </p>
+    <div class="login">
+      <img src="@/assets/DCHQ-dark.svg" alt>
+      <h4>Sign into Design+Code HQ</h4>
+      <input type="email" placeholder="Email">
+      <input type="password" placeholder="Password">
+      <button>Sign in</button>
+      <router-link to="/recover">Forgot your password?</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn"
+};
+</script>
+
+<style scoped lang="sass">
+  .container
+    display: flex
+    justify-content: center
+    align-items: center
+    min-height: 100vh
+
+  .request
+    margin: 0
+    position: absolute
+    top: 24px
+    right: 24px
+    color: rgba(255, 255, 255, 0.3)
+    a
+      color: white
+
+  .login
+    width: 400px
+
+  h4
+    margin: 0
+    line-height: 34px
+    font-size: 24px
+    text-align: center
+    color: #FFFFFF
+  
+  input
+    background: rgba(255, 255, 255, 0.2)
+    border: 1px solid rgba(255, 255, 255, 0.2)
+    box-sizing: border-box;
+    border-radius: 4px;
+    height: 60px
+    width: 100%
+    font-size: 20px
+    color: white
+    padding-left: 20px
+    margin-top: 20px
+    &::placeholder
+      color: rgba(255, 255, 255, 0.3)
+
+  button
+    background: #56CCF2
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2)
+    border-radius: 4px
+    height: 60px
+    width: 100%
+    font-size: 20px
+    margin-top: 20px
+    border: none
+    margin-bottom: 40px
+
+  a
+    line-height: 25px
+    text-align: center
+    color: rgba(255, 255, 255, 0.3)
+    text-decoration: none
+
+</style>
